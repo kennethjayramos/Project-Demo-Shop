b49c9c7056b64970c61241905d4cd340757d51b1
kenneth.jay.ramos <kenneth.jay.ramos@gmail.com>
2020-08-27 10:15:28 +0800

    Initial VS 2019 Solution & Project Setup

---
+++ Project-Demo-Shop/Demo-Shop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Demo_Shop.Startup))]
+namespace Demo_Shop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
