58acca3176f49cbea0b66d0e6a0a46fe4c5f54e4
kenneth.jay.ramos <kenneth.jay.ramos@gmail.com>
2020-08-31 16:44:53 +0800

    Added Basket & BasketItem models, updated the database by EF

---
+++ Project-Demo-Shop/Demo-Shop/Core/Models/Basket.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Demo_Shop.Core.Models
+{
+    public class Basket : BaseEntity
+    {
+        // Lay loading of basktitems
+        public virtual ICollection<BasketItem> BasketItems { get; set; }
+
+        #region Constructor
+        public Basket()
+        {
+            this.BasketItems = new List<BasketItem>();
+        }
+        #endregion
+    }
+}
--- Project-Demo-Shop/Demo-Shop/DataAccess/Sql/ProductContext.cs
+++ Project-Demo-Shop/Demo-Shop/DataAccess/Sql/ProductContext.cs
@@ -13,5 +13,9 @@ namespace Demo_Shop.DataAccess.Sql
         public DbSet<Product> Products { get; set; }
 
         public DbSet<ProductCategory> ProductCategories { get; set; }
+
+        public DbSet<Basket> Baskets { get; set; }
+
+        public DbSet<BasketItem> BasketItems { get; set; }
     }
 }
